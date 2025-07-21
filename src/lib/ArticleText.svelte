e30541edafd3119e0673986b1e16a3921121c5db
Melly Mejia <mellymejia@Mellys-MacBook-Air-2.local>
2025-07-21 15:48:49 -0400

    ArticleText css

--- src/lib/ArticleText.svelte
+++ src/lib/ArticleText.svelte
@@ -12,11 +12,11 @@
   .article-text {
     margin: 50vh auto;
     width: 50%;
-    background-color: #ff99fc;
-    color: #007052;
-    border: solid #8427c9 3px;
+    background-color: #f8f5ed;
+    color: Black;
+    border: solid #a1261d 3px;
     border-radius: 20px;
     padding: 20px;
-    box-shadow: 16px 16px #8aa6df;
+    box-shadow: 16px 16px #737373;
   }
 </style>
